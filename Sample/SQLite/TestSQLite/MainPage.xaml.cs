cd25218dc010096586db93084c3f3d58d2f77e0a
Keming <believe75467@gmail.com>
2012-12-21 14:26:40 +0800

    Add SQLite example

---
+++ Sample/SQLite/TestSQLite/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白頁項目範本已記錄在 http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestSQLite
+{
+    /// <summary>
+    /// 可以在本身使用或巡覽至框架內的空白頁面。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        string dbPath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqlite.db");
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadData();
+        }
+
+        /// <summary>
+        /// 在此頁面即將顯示在框架中時叫用。
+        /// </summary>
+        /// <param name="e">描述如何到達此頁面的事件資料。Parameter
+        /// 屬性通常用來設定頁面。</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+        
+        public void LoadData()
+        {
+            SQLiteConnection sqlite_conn = new SQLiteConnection(dbPath, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite, true);
+
+            sqlite_conn.CreateTable<Person>();
+
+            sqlite_conn.Insert(new Person() { FirstName = "Keming", LastName = "Chen" });
+
+            List<Person> personList = sqlite_conn.Query<Person>("SELECT * FROM Person");
+
+            for (int i = 0; i < personList.Count; i++)
+            {
+                _myListBox.Items.Add(personList[i].FirstName + " " + personList[i].LastName);
+            }
+
+            sqlite_conn.Close();
+        }
+
+        private void ClickTestButton(object sender, RoutedEventArgs e)
+        {
+            App.Current.Exit();
+        }
+    }
+}
---
+++ Sample/SQLite/TestSQLite/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSQLite
+{
+    public class Person
+    {
+        [SQLite.AutoIncrement, SQLite.PrimaryKey]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
