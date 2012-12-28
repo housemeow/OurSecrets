29865c6bc0d8de15211bf795b631744e8a1099dc
Kai <housemeow@gmail.com>
2012-12-28 16:11:55 +0800

    fix bug

--- OurSecrets/Agendas.cs
+++ OurSecrets/Agendas.cs
@@ -7,10 +7,69 @@ namespace OurSecrets
 {
     public class Agendas
     {
+        private List<Agenda> _agendaList;
+
         private static int _newAgendaID;
         public static int GetNewAgendaID()
         {
             return _newAgendaID++;
+        }
+
+        public Agendas()
+        {
+            _agendaList = new List<Agenda>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _agendaList.Count;
+            }
+        }
+
+        public void AddAgenda(Agenda agenda)
+        {
+            _agendaList.Add(agenda);
+        }
+
+        public List<Agenda> GetAgendaList(DateTime? dateTime)
+        {
+            List<Agenda> resultAgenda = new List<Agenda>();
+            foreach (Agenda agenda in _agendaList)
+            {
+                if (dateTime == null ^ agenda.StartDateTime == null)
+                {
+                    continue;
+                }
+                else if (agenda.StartDateTime <= dateTime && agenda.EndDateTime >= dateTime)
+                {
+                    resultAgenda.Add(agenda);
+                }
+            }
+            return resultAgenda;
+        }
+
+        public List<Agenda> GetAgendaList(DateTime startDateTime, DateTime endDateTime)
+        {
+            List<Agenda> resultAgenda = new List<Agenda>();
+            foreach (Agenda agenda in _agendaList)
+            {
+                if (startDateTime == null ^ agenda.StartDateTime == null)
+                {
+                    continue;
+                }
+
+                else if (startDateTime >= agenda.StartDateTime && agenda.StartDateTime <= endDateTime)
+                {
+                    resultAgenda.Add(agenda);
+                }
+                else if (startDateTime >= agenda.EndDateTime && agenda.EndDateTime <= endDateTime)
+                {
+                    resultAgenda.Add(agenda);
+                }
+            }
+            return resultAgenda;
         }
     }
 }
---
+++ TestProject/AgendasTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using OurSecrets;
+
+namespace TestProject
+{
+    [TestClass]
+    public class AgendasTest
+    {
+        private Agendas _agendas;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _agendas = new Agendas();
+        }
+
+        [TestMethod]
+        public void TestConstructor()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAddAgenda()
+        {
+            Agenda agenda = new Agenda();
+            _agendas.AddAgenda(agenda);
+            Assert.AreEqual(1, _agendas.Count);
+            _agendas.AddAgenda(agenda);
+            Assert.AreEqual(2, _agendas.Count);
+        }
+
+        [TestMethod]
+        public void TestGetAgendasByDateTime()
+        {
+            DateTime dateTime1 = new DateTime(2012, 3, 15);
+            DateTime dateTime2 = new DateTime(2012, 4, 11);
+            Agenda agenda1 = new Agenda(dateTime1);
+            agenda1.Title = "dateTime1";
+            Agenda agenda2 = new Agenda(dateTime2);
+            agenda2.Title = "dateTime2";
+            Agenda agenda3 = new Agenda(dateTime1);
+            agenda3.Title = "dateTime1";
+            Agenda agenda4 = new Agenda(dateTime2);
+            agenda4.Title = "dateTime2";
+            Agenda agenda5 = new Agenda(dateTime1);
+            agenda5.Title = "dateTime1";
+            _agendas.AddAgenda(agenda1);
+            _agendas.AddAgenda(agenda2);
+            _agendas.AddAgenda(agenda3);
+            _agendas.AddAgenda(agenda4);
+            _agendas.AddAgenda(agenda5);
+            List<Agenda> agendaList = _agendas.GetAgendaList(dateTime1);
+            Assert.AreEqual(3, agendaList.Count);
+            foreach (Agenda agenda in agendaList)
+            {
+                Assert.AreEqual("dateTime1", agenda.Title);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetAgendaListByDuration()
+        {
+            DateTime startDateTime = new DateTime(2012, 1, 2);
+            DateTime endDateTime = new DateTime(2012, 1, 4); 
+            Agenda agenda1 = new Agenda(new DateTime(2012, 1, 1));
+            agenda1.Title = "agenda1";
+            Agenda agenda2 = new Agenda(startDateTime);
+            agenda2.Title = "agenda2";
+            Agenda agenda3 = new Agenda(new DateTime(2012, 1, 3));
+            agenda3.Title = "agenda3";
+            Agenda agenda4 = new Agenda(endDateTime);
+            agenda4.Title = "agenda4";
+            Agenda agenda5 = new Agenda(new DateTime(2012, 1, 5));
+            agenda5.Title = "agenda5";
+            _agendas.AddAgenda(agenda1);
+            _agendas.AddAgenda(agenda2);
+            _agendas.AddAgenda(agenda3);
+            _agendas.AddAgenda(agenda4);
+            _agendas.AddAgenda(agenda5);
+            List<Agenda> agendaList = _agendas.GetAgendaList(startDateTime, endDateTime);
+            Assert.AreEqual(3, agendaList.Count);
+            Assert.AreEqual("2", agendaList[0].Title);
+            Assert.AreEqual("3", agendaList[1].Title);
+            Assert.AreEqual("4", agendaList[2].Title);
+        }
+    }
+}
