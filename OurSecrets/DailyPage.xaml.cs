b1dd5a822d53b8eedca9155f268df2922153f65b
Kai <housemeow@gmail.com>
2012-12-30 08:39:44 +0800

    add calendar

--- OurSecrets/DailyPage.xaml.cs
+++ OurSecrets/DailyPage.xaml.cs
@@ -37,6 +37,12 @@ namespace OurSecrets
             AddView.DragItemsStarting += View_DragItemsStarting;
             Window.Current.CoreWindow.PointerWheelChanged += CoreWindow_PointerWheelChanged;
             //Window.Current.CoreWindow.PointerPressed += CoreWindow_PointerPressed;
+            App.AgendasModel.PropertyChanged += AgendasModel_PropertyChanged;
+        }
+
+        void AgendasModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            ;
         }
 
         void CoreWindow_PointerPressed(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.PointerEventArgs args)
